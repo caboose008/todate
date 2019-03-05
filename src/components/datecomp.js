6af82124845526831282a1fd792bab1cbfec0e5b
caboose008 <32422426+caboose008@users.noreply.github.com>
2019-03-05 15:34:01 -0800

    Adjacted text style for the mobile
    Adjacted text style and size for mobile phone. Added label of calendar.

--- src/components/datecomp.js
+++ src/components/datecomp.js
@@ -4,11 +4,17 @@ import React from 'react';
 //when date is selected, the updateOnDateChange method is triggered
 export default class DateComp extends React.Component{
 	render(){		
-		return (<div className="row">
+		return (<div><div className="row">
 					<div className="col-lg-4 col-md-4"></div>
 					<div className="col-lg-4 col-md-4">
-						<input type="date" id="date" onChange={this.props.updateOnDateChange}/>
+						<p>Select a date</p>
 					</div>	
-				</div>);
+				</div>
+				<div className="row">
+					<div className="col-lg-4 col-md-4"></div>
+					<div className="col-lg-4 col-md-4">
+						<input className="calendar" type="date" id="date" onChange={this.props.updateOnDateChange}/>
+					</div>	
+				</div></div>);
 	}
 }
